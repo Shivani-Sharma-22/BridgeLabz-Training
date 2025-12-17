90ada08ca69144046c8848624bc0dfe65a167b9d
shivani-sharma-22 <shivanisharma3081@gmail.com>
2025-12-17 21:30:09 +0530

    [Shivani Sharma] Area of circle

---
+++ core-java-practice/gcr-codebase/programming-elements/AreaOfCircle.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class AreaOfCircle {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int radius=sc.nextInt();
+        int area=(int) (3.14*radius*radius);
+        System.out.print("Area of Circle: "+area);
+    
+    }
+}
