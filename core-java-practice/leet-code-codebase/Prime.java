d24106179c11981751cf1fa4cfe3c529d4bca5ec
shivani-sharma-22 <shivanisharma3081@gmail.com>
2025-12-17 15:30:30 +0530

    added a prime number code

---
+++ core-java-practice/leet-code-codebase/Prime.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Prime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //input a number
+        int num = sc.nextInt();
+
+        if (num <= 1) {
+            System.out.println(num + " is NOT a prime number");
+            return;
+        }
+
+        boolean isPrime = true;
+
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime)
+            System.out.println(num + " is a PRIME number");
+        else
+            System.out.println(num + " is NOT a prime number");
+
+        sc.close();
+    }
+}
