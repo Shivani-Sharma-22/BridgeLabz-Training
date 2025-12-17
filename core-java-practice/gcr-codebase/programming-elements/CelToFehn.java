b39f45fe4ba3b8ec4863cc8708353fddd1e2dd4b
shivani-sharma-22 <shivanisharma3081@gmail.com>
2025-12-17 21:27:25 +0530

    [Shivani Sharma] Celcius to Fahrenheit

---
+++ core-java-practice/gcr-codebase/programming-elements/CelToFehn.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class CelToFehn {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int tempC = sc.nextInt();
+        int Fahrenheit = (tempC * 9/5)+32;
+        System.out.print(Fahrenheit);
+    
+    }
+}
