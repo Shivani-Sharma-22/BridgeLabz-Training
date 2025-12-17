3ac1da86dd15e8b59281739d0e6f6edcf2c9a0c3
shivani-sharma-22 <shivanisharma3081@gmail.com>
2025-12-17 21:22:54 +0530

    [Shivani Sharma] power calculation

---
+++ core-java-practice/gcr-codebase/programming-elements/PowerCalculation.java
@@ -0,0 +1,11 @@
+
+import java.util.*;
+public class PowerCalculation {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int value=sc.nextInt();
+        int power=sc.nextInt();
+        int powerCal=(int)Math.pow(value,power);
+        System.out.println(powerCal);
+    }
+}
