a6af793190a2af98bfcdca8c50faf8461309178b
shivani-sharma-22 <shivanisharma3081@gmail.com>
2025-12-17 14:46:01 +0530

    Added new changes

--- core-java-practice/leet-code-codebase/palindrome.java
+++ core-java-practice/leet-code-codebase/palindrome.java
@@ -3,8 +3,9 @@ class palindrome{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
+        System.out.println(isPalindrome(x));
     }
-    public boolean isPalindrome(int x) {
+    public static boolean isPalindrome(int x) {
         int temp = x;
         int rev =0;
         while(x> 0){
