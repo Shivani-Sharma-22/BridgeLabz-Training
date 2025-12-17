8c1bd6cb2729b29999fbe813a4478b7912fa42ce
shivani-sharma-22 <shivanisharma3081@gmail.com>
2025-12-17 21:31:15 +0530

    [Shivani Sharma] Add Two Number

---
+++ core-java-practice/gcr-codebase/programming-elements/AddToNum.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class AddToNum {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int num1=sc.nextInt();
+        int num2=sc.nextInt();
+        System.out.print("Sum of two no.: "+(num1+num2));
+    
+    }
+}
