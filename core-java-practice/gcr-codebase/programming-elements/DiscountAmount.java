80f50e08802986870d2733c6941f8606ebcc97e7
shivani-sharma-22 <shivanisharma3081@gmail.com>
2025-12-18 11:34:07 +0530

    [Shivani Sharma] Calculated fee after discount

---
+++ core-java-practice/gcr-codebase/programming-elements/DiscountAmount.java
@@ -0,0 +1,10 @@
+public class DiscountAmount {
+    public static void main(String[] args) {
+        int fee = 125000;
+        int discountPercentage = 10;
+        int calDiscount = (fee/discountPercentage);
+        int payFees = (fee - calDiscount);
+        System.out.println("The discount amount is INR "+ calDiscount+" and final discounted fee is INR "+ payFees);
+
+    }
+}
