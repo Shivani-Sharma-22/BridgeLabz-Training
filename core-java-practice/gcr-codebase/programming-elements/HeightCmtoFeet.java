89a489fcc86464fd6e148e02349e0c3305a1f1b7
shivani-sharma-22 <shivanisharma3081@gmail.com>
2025-12-18 12:54:25 +0530

    [Shivani Sharma] height (CM) to Feet and Inches

---
+++ core-java-practice/gcr-codebase/programming-elements/HeightCmtoFeet.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class HeightCmtoFeet {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int height = input.nextInt();
+        double CmPerInch = 2.54;
+        int InchedPerFoot = 12;
+
+         // Converting centimeters to total inches
+         double totalInches = height / CmPerInch;
+
+         // Calculating feet and remaining inches
+         int heightInFeet = (int) (totalInches / InchedPerFoot);
+         double remainingInches = totalInches % InchedPerFoot;
+ 
+         // Displaying the result
+         System.out.println("Your height in cm is " + height +
+                 " while in feet is " + heightInFeet +
+                 " and inches is " + remainingInches);
+    }
+}
