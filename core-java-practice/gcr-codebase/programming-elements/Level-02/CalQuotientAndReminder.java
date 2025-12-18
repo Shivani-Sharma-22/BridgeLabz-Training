fde54f02f6aa87d7d83991b9bfb08af718e7b113
shivani-sharma-22 <shivanisharma3081@gmail.com>
2025-12-18 15:59:44 +0530

    [Shivani Sharma] Calculate the quotient and remainder

---
+++ core-java-practice/gcr-codebase/programming-elements/Level-02/CalQuotientAndReminder.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class CalQuotientAndReminder{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        //Taking first number input from user
+        int number1 = input.nextInt();
+
+        // Taking second number input from user
+        int number2 = input.nextInt();
+
+        // Calculating quotient and remainder
+        int quotient = number1 / number2;
+        int remainder = number1 % number2;
+
+        // Displaying the result
+        System.out.println("The Quotient is " + quotient +
+                " and Reminder is " + remainder +
+                " of two number " + number1 + " and " + number2);
+    }
+}
