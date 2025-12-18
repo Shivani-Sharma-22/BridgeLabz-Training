0aca107ff014737c626e1dcd7a8a54e760a32cd3
shivani-sharma-22 <shivanisharma3081@gmail.com>
2025-12-18 11:27:01 +0530

    [Shivani Sharma] Distributed pens per student

---
+++ core-java-practice/gcr-codebase/programming-elements/DividedPens.java
@@ -0,0 +1,10 @@
+public class DividedPens {
+    public static void main(String[] args) {
+        int pens = 14;
+        int student = 3;
+        int divOfPen = pens/3;
+        int totaldistruted = (divOfPen+divOfPen+divOfPen);
+        int remaining = pens - totaldistruted;
+        System.out.println("The pen Per Student is "+ divOfPen +" and the reamining pens not distributed is "+remaining);
+    }
+}
