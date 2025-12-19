af82c9dfd65120664b6cb0450ddeac2fd50c71f1
Shivani Sharma <shivanisharma3081@gmail.com>
2025-12-19 15:18:31 +0530

    [Shivani Sharma] Control flow problem

--- core-java-practice/gcr-codebase/programming-elements/Level-02/IntOperation .java
+++ core-java-practice/gcr-codebase/programming-elements/Level-02/IntOperation .java
@@ -7,6 +7,6 @@ class IntOperation   {
         int b = input.nextInt();
         int c = input.nextInt();
 
-        System.out.println("The results of Int Operations are "+(a + b *c)+" "+(a * b + c)+" "+(c + a / b)+"and "+(a % b + c));
+        System.out.println("The results of Int Operations are "+(a + b *c)+" "+(a * b + c)+" "+(c + a / b)+" and "+(a % b + c));
     }
 }
