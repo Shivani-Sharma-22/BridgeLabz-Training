911f597556fff54bb82bd4dd26b0083cb8bbb039
Shivani Sharma <shivanisharma3081@gmail.com>
2025-12-19 16:13:49 +0530

    [Shivani Sharma] Add . find factor of a number

---
+++ core-java-practice/gcr-codebase/control-flow/FindFactorOfNumber.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class FindFactorOfNumber {
+    public static void main(String[] args) {
+        //creating scanner object
+        Scanner sc = new Scanner(System.in);
+
+        //taking input from user
+        int number = sc.nextInt();
+
+        if(number >0){
+
+            for (int i = 1; i <= number; i++) {
+                if (number % i == 0) {
+                    System.out.println(i);
+                }
+            }
+        }
+    }
+}
