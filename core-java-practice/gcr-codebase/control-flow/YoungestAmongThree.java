6526b3c8cab574018ed43c37c23166aace8c851b
Shivani Sharma <shivanisharma3081@gmail.com>
2025-12-19 16:04:01 +0530

    [Shivani Sharma] Add . youngest and tallest person among three

---
+++ core-java-practice/gcr-codebase/control-flow/YoungestAmongThree.java
@@ -0,0 +1,43 @@
+
+import java.util.*;
+public class YoungestAmongThree {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //taking input salary,year from user
+        System.out.println("Enter age of Amar");
+        int ageOfAmar = sc.nextInt();
+        System.out.println("Enter age of Akbar");
+        int ageOfAkbar = sc.nextInt();
+        System.out.println("Enter age of Anothy");
+        int ageOfAnthony  = sc.nextInt();
+        System.out.println("Enter height of Amar");
+        int heightOfAmar = sc.nextInt();
+        System.out.println("Enter height of Akbar");
+        int heightOfAkbar = sc.nextInt();
+        System.out.println("Enter height of Anothy");
+        int heightOfAnthony  = sc.nextInt();
+        
+        //find youngest person among three person
+        if(ageOfAmar < ageOfAkbar && ageOfAmar < ageOfAnthony ){
+            System.out.println("The aged person is Amar");
+        }
+        else if(ageOfAkbar < ageOfAmar && ageOfAkbar < ageOfAnthony){
+            System.out.println("The aged person is Akbar ");
+        }
+        else{
+            System.out.println("The aged person is Anothy");
+        }
+        //find the tallest person
+        if(heightOfAmar > heightOfAkbar && heightOfAmar > heightOfAnthony ){
+            System.out.println("The Tallest person is Amar");
+        }
+        else if(heightOfAkbar > heightOfAmar && heightOfAkbar > heightOfAnthony){
+            System.out.println("The Tallest person is Akbar ");
+        }
+        else{
+            System.out.println("The Tallest person is Anothy");
+        }
+        
+    }
+}
