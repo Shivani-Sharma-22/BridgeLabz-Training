d0af523c75be251b0a0d540a4816900cab5cb614
Shivani Sharma <shivanisharma3081@gmail.com>
2025-12-20 22:55:18 +0530

    [shivani sharma] Add.solved arrays-Level-01 Problem

---
+++ core-java-practice/gcr-codebase/arrays/BMICalculation.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class BMICalculation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int persons = sc.nextInt();
+        double[] weight = new double[persons];
+        double[] height = new double[persons];
+        double[] bmi = new double[persons];
+        String[] status = new String[persons];
+        for (int i = 0; i < persons; i++) {
+            weight[i] = sc.nextDouble();
+            height[i] = sc.nextDouble();
+        }
+        for (int i = 0; i<persons;i++) {
+            double hMeter = height[i] / 100;
+            bmi[i] = weight[i] / (hMeter * hMeter);
+            if (bmi[i] <= 18.4) {
+                status[i]= "Underweight";
+            } else if(bmi[i] <= 24.9) {
+                status[i] = "Normal";
+            } else if(bmi[i] <=39.9) {
+                status[i] = "Overweight";
+            } else{
+                status[i]="Obese";
+            }
+        }
+        for (int i=0;i< persons;i++) {
+            System.out.println("Height " + height[i]+" Weight " + weight[i]+" BMI " + bmi[i]+" Status " + status[i]);
+        }
+        sc.close();
+    }
+}
