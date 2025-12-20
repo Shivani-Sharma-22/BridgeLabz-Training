34989df59003541a3fd3f66a2841fae4aabe9b9a
Shivani Sharma <shivanisharma3081@gmail.com>
2025-12-20 22:16:26 +0530

    [Shivani Sharma] Add.solved arrays-Level-01 Problem

--- core-java-practice/gcr-codebase/arrays/Multiplevalues.java
+++ core-java-practice/gcr-codebase/arrays/Multiplevalues.java
@@ -3,21 +3,21 @@ import java.util.Scanner;
 public class Multiplevalues {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double[] values=new double[10];
-        double total =0.0;
-        int index= 0;
+        double[] values = new double[10];
+        double total = 0.0;
+        int index = 0;
         while (true) {
-            double input= sc.nextDouble();
-            if (input <= 0|| index==10) {
+            double input = sc.nextDouble();
+            if (input <= 0 || index == 10) {
                 break;
             }
-            values[index]=input;
+            values[index] = input;
             index++;
         }
-        for (int i =0; i <index; i++) {
-            total = total+values[i];
+        for (int i = 0; i < index; i++) {
+            total = total + values[i];
         }
         System.out.println("Sum is " + total);
-    
+        sc.close();
     }
 }
