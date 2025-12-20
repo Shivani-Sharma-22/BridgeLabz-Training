f8a0f5801d5b16c13a74e9b6c24348101dcbd307
Shivani Sharma <shivanisharma3081@gmail.com>
2025-12-20 11:44:37 +0530

    [Shivani Sharma] Add.ControlFlowLevel1

---
+++ core-java-practice/gcr-codebase/control-flow/SpringSeason.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class SpringSeason {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+		int date=sc.nextInt();
+		int month=sc.nextInt();
+		
+			if(month==3 &&  date>=20) {
+				System.out.println("It is a Spring Season");
+			}
+			else if(month==6 || date<=20) {
+				System.out.println("Its a Spring Season");
+			}
+			else if(month>3 && month<6) {
+				System.out.println("Its a Spring Season");
+			}
+			else {
+				System.out.println("Not a Spring Season");
+			}
+    }
+}
