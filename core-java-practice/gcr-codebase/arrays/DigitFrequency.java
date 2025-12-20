2d78a0a892759d7b0554a67975b2dc6cf7d561c1
Shivani Sharma <shivanisharma3081@gmail.com>
2025-12-20 23:01:43 +0530

    [shivani sharma] Add.solved arrays-Level-01 Problem

---
+++ core-java-practice/gcr-codebase/arrays/DigitFrequency.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class DigitFrequency {
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+
+        int number = sc.nextInt();
+        int temp = number;
+
+        int count = 0;
+
+        // count digits
+        while (temp != 0) {
+            count++;
+            temp = temp / 10;
+        }
+
+        int[] digits = new int[count];
+        int index = 0;
+
+        // store digits in array
+        while (number != 0) {
+            digits[index] = number % 10;
+            number = number / 10;
+            index++;
+        }
+
+        int[] frequency = new int[10];
+
+        // find frequency
+        for (int i = 0; i < digits.length; i++) {
+            frequency[digits[i]]++;
+        }
+
+        // display frequency
+        for (int i = 0; i < frequency.length; i++) {
+            if (frequency[i] > 0) {
+                System.out.println("Digit " + i + " occurs " + frequency[i] + " times");
+            }
+        }
+
+        sc.close();
+    }
+}
