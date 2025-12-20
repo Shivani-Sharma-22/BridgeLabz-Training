d07ca832a4ebf453aa52315a2e92212e4e77c653
Shivani Sharma <shivanisharma3081@gmail.com>
2025-12-20 11:39:34 +0530

    [Shivani Sharma] Control-flow-Level-01-Problem

---
+++ core-java-practice/gcr-codebase/control-flow/CheckPosNegZero.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class CheckPosNegZero {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //taking input a number from user
+        int num = sc.nextInt();
+
+        //check for positive number
+        if(num >0){
+            System.out.println("Positive");
+        }
+        else if(num == 0){
+            System.out.println("Zero");
+        }
+        else{
+            System.out.println("Negative");
+        }
+    }
+}
