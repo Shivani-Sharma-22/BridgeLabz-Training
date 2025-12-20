c476d31f634632e9dcd923ab99b73a9c1d33335e
Shivani Sharma <shivanisharma3081@gmail.com>
2025-12-20 23:05:21 +0530

    [shivani sharma] Add.solved arrays-Level-01 Problem

--- core-java-practice/gcr-codebase/arrays/DigitFrequency.java
+++ core-java-practice/gcr-codebase/arrays/DigitFrequency.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class DigitFrequency {
     public static void main(String[] args) {
-         Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         int number = sc.nextInt();
         int temp = number;
