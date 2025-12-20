f481bdf6f50e4438fb26c9de7ade1df4b1f4192b
Shivani Sharma <shivanisharma3081@gmail.com>
2025-12-20 11:34:06 +0530

    [Shivani Sharma] Control-flow-Level-01-Problem

---
+++ core-java-practice/gcr-codebase/control-flow/CanTheyVote.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class CanTheyVote {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //taking input age from user
+        int age = sc.nextInt();
+
+        //check for vote
+        if(age >= 18){
+            System.out.println("The person can vote");
+        }else{
+            System.out.println("The person cannot vote.");
+        }
+    }
+}
