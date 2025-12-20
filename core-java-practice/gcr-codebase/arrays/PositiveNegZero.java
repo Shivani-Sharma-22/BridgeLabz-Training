e2fcbb34e728238d492c6d65b6bc3340c92c3665
Shivani Sharma <shivanisharma3081@gmail.com>
2025-12-20 15:33:27 +0530

    [Shivani Sharma] Add.solved arrays-Level-01 Problem

---
+++ core-java-practice/gcr-codebase/arrays/PositiveNegZero.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class PositiveNegZero {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n =5;
+        //array
+        int[] arr = new int[n];
+        for(int i =0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i =0;i<arr.length;i++){
+            if(arr[i] > 0){
+                if(arr[i] % 2==0){
+                    System.out.println("Even number: "+ arr[i]);
+                }
+                else{
+                    System.out.println("Odd number: "+ arr[i]);
+                }
+            }
+            else if(arr[i] == 0){
+                System.out.println("Zero");
+            }
+            else {
+                System.out.println("Negative Number");
+            }
+            if(arr[i] == arr[n-1]){
+                System.out.println("equal");
+            }
+            else if(arr[i] < arr[n-1]){
+                System.out.println("less than");
+            }
+            else {
+                System.out.println("greater ");
+            }
+        }
+    }
+}
