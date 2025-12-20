5f0645b95e542964bc0ca61006d0be928360b772
Shivani Sharma <shivanisharma3081@gmail.com>
2025-12-20 22:36:35 +0530

    [shivani sharma] Add.solved arrays-Level-01 Problem

---
+++ core-java-practice/gcr-codebase/arrays/OddEvenArray.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class OddEvenArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        // Natural number check
+        if (number <= 0) {
+            System.out.println("Error: Please enter a natural number");
+            return;
+        }
+
+        int size = number / 2 + 1;
+        int[] even = new int[size];
+        int[] odd = new int[size];
+
+        int evenIndex = 0;
+        int oddIndex = 0;
+
+        // Store odd and even numbers
+        for (int i = 1; i <= number; i++) {
+            if (i % 2 == 0) {
+                even[evenIndex] = i;
+                evenIndex++;
+            } else {
+                odd[oddIndex] = i;
+                oddIndex++;
+            }
+        }
+
+        // Print even array
+        System.out.println("Even Numbers:");
+        for (int i = 0; i < evenIndex; i++) {
+            System.out.print(even[i] + " ");
+        }
+        System.out.println();
+        // Print odd array
+        System.out.println("Odd Numbers:");
+        for (int i = 0; i < oddIndex; i++) {
+            System.out.print(odd[i] + " ");
+        }
+
+        sc.close();
+    }
+}
