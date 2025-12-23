db9aa2d735f9f23ed1e551831afd157d7f130939
Shivani Sharma <shivanisharma3081@gmail.com>
2025-12-23 12:48:49 +0530

    [Shivani Sharma]level-03

---
+++ corejavapractice/gcrcodebase/strings/CharacterFrequencyUsingUnique.java
@@ -0,0 +1,69 @@
+package gcrcodebase.strings;
+
+import java.util.*;
+public class CharacterFrequencyUsingUnique {
+	 public static char[] uniqueCharacters(String text) {
+
+	        char[] temp = new char[text.length()];
+	        int count = 0;
+
+	        for (int i = 0; i < text.length(); i++) {
+	            char ch = text.charAt(i);
+	            boolean found = false;
+
+	            for (int j = 0; j < count; j++) {
+	                if (temp[j] == ch) {
+	                    found = true;
+	                    break;
+	                }
+	            }
+
+	            if (!found) {
+	                temp[count++] = ch;
+	            }
+	        }
+
+	        char[] unique = new char[count];
+	        for (int i = 0; i < count; i++) {
+	            unique[i] = temp[i];
+	        }
+
+	        return unique;
+	    }
+	 public static String[][] findFrequency(String text) {
+
+	        int[] freq = new int[256];
+
+	        for (int i = 0; i < text.length(); i++) {
+	            freq[text.charAt(i)]++;
+	        }
+
+	        char[] unique = uniqueCharacters(text);
+	        String[][] result = new String[unique.length][2];
+
+	        for (int i = 0; i < unique.length; i++) {
+	            result[i][0] = String.valueOf(unique[i]);
+	            result[i][1] = String.valueOf(freq[unique[i]]);
+	        }
+
+	        return result;
+	    }
+	 public static void main(String[] args) {
+
+	        Scanner sc = new Scanner(System.in);
+
+	        System.out.print("Enter a string: ");
+	        String text = sc.nextLine();
+
+	        String[][] data = findFrequency(text);
+
+	        System.out.println("Character\tFrequency");
+	        System.out.println("--------------------");
+
+	        for (int i = 0; i < data.length; i++) {
+	            System.out.println(data[i][0] + "\t\t" + data[i][1]);
+	        }
+
+	        sc.close();
+	    }
+}
