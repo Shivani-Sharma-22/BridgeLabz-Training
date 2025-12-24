98a680d32a7d68516f897ae4587aeed1e355a5ce
Shivani Sharma <shivanisharma3081@gmail.com>
2025-12-24 12:58:57 +0530

    [Shivani Sharma]java-methods(Level-01)

---
+++ corejavapractice/gcrcodebase/methods/PositiveNegZero.java
@@ -0,0 +1,23 @@
+package gcrcodebase.methods;
+
+import java.util.*;
+public class PositiveNegZero {
+	static String posNegZero(int num) {
+		if(num < 0) {
+			return "Negative";
+		}
+		else if(num == 0) {
+			return "Zero";
+		}
+		return "Positive";
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		//taking number as input 
+		System.out.println("Eneter the number:");
+		int num = sc.nextInt();
+		System.out.println(posNegZero(num));
+		
+	}
+}
