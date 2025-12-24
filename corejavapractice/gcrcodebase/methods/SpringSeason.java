c1c5a7abeb91ca760072bb8ee07dbd8a4e8f6f08
Shivani Sharma <shivanisharma3081@gmail.com>
2025-12-24 13:14:18 +0530

    [Shivani Sharma]java-methods(Level-01)

---
+++ corejavapractice/gcrcodebase/methods/SpringSeason.java
@@ -0,0 +1,27 @@
+package gcrcodebase.methods;
+
+import java.util.*;
+public class SpringSeason {
+	public static void springSeason(int day,int month) {
+		
+		if(month == 3 && day >= 20) {
+			System.out.println("It is a Spring Season");
+		}
+		else if(month > 3 && month < 6) {
+			System.out.println("It is  a Spring Season");
+		}
+		else if(month == 6 && day <= 20){
+			System.out.println("It is a Spring Season");
+		}
+		else {
+			System.out.println("It is not a Spring Season");
+		}
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int day = sc.nextInt();
+		int month = sc.nextInt();
+		springSeason(day,month);
+	}
+}
