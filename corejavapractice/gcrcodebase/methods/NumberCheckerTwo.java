4b0fcd4374d2181217a17634b98a398e7d91e71a
Shivani Sharma <shivanisharma3081@gmail.com>
2025-12-25 22:18:54 +0530

    [Shivani Sharma]java-methods(Level-03)

---
+++ corejavapractice/gcrcodebase/methods/NumberCheckerTwo.java
@@ -0,0 +1,66 @@
+package gcrcodebase.methods;
+
+public class NumberCheckerTwo {
+
+	 // Count digits
+    public static int countDigits(int number) {
+        int count = 0;
+        while (number != 0) {
+            count++;
+            number /= 10;
+        }
+        return count;
+    }
+
+    // Store digits in array
+    public static int[] getDigits(int number) {
+        int size = countDigits(number);
+        int[] digits = new int[size];
+
+        for (int i = size - 1; i >= 0; i--) {
+            digits[i] = number % 10;
+            number /= 10;
+        }
+        return digits;
+    }
+
+    // Sum of digits
+    public static int sumOfDigits(int[] digits) {
+        int sum = 0;
+        for (int d : digits) {
+            sum += d;
+        }
+        return sum;
+    }
+
+    // Sum of squares of digits
+    public static int sumOfSquares(int[] digits) {
+        int sum = 0;
+        for (int d : digits) {
+            sum += Math.pow(d, 2);
+        }
+        return sum;
+    }
+
+    // Harshad number check
+    public static boolean isHarshad(int number, int[] digits) {
+        int sum = sumOfDigits(digits);
+        return number % sum == 0;
+    }
+
+    // Frequency of digits (2D array)
+    public static int[][] digitFrequency(int[] digits) {
+
+        int[][] freq = new int[10][2];
+
+        for (int i = 0; i < 10; i++) {
+            freq[i][0] = i;
+            freq[i][1] = 0;
+        }
+
+        for (int d : digits) {
+            freq[d][1]++;
+        }
+        return freq;
+    }
+}
