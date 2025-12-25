e6d0bab1d97bd34636f6f8cfa566477f4fdbb6be
Shivani Sharma <shivanisharma3081@gmail.com>
2025-12-25 23:32:32 +0530

    [Shivani Sharma]java-practice-problem

---
+++ corejavapractice/gcrcodebase/practiceproblem/LongestWord.java
@@ -0,0 +1,21 @@
+package gcrcodebase.practiceproblem;
+
+import java.util.*;
+public class LongestWord {
+	 public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        System.out.print("Enter sentence: ");
+	        String s = sc.nextLine();
+
+	        String[] words = s.split(" ");
+	        String longest = words[0];
+
+	        for (String w : words) {
+	            if (w.length() > longest.length())
+	                longest = w;
+	        }
+
+	        System.out.println("Longest word: " + longest);
+	    }
+
+}
