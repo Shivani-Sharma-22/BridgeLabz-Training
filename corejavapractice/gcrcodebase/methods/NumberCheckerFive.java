0136df11190b9c7799c44a864b757abfd78b95d4
Shivani Sharma <shivanisharma3081@gmail.com>
2025-12-25 23:04:40 +0530

    [Shivani Sharma]java-methods(Level-03)

---
+++ corejavapractice/gcrcodebase/methods/NumberCheckerFive.java
@@ -0,0 +1,109 @@
+package gcrcodebase.methods;
+
+public class NumberCheckerFive {
+
+    // ---------- PERFECT / ABUNDANT / DEFICIENT ----------
+    public static int sumOfProperDivisors(int num) {
+        int sum = 0;
+        for (int i = 1; i <= num / 2; i++) {
+            if (num % i == 0) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+
+    public static boolean isPerfectNumber(int num) {
+        return sumOfProperDivisors(num) == num;
+    }
+
+    public static boolean isAbundantNumber(int num) {
+        return sumOfProperDivisors(num) > num;
+    }
+
+    public static boolean isDeficientNumber(int num) {
+        return sumOfProperDivisors(num) < num;
+    }
+
+    // ---------- STRONG NUMBER ----------
+    private static int factorial(int n) {
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
+
+    public static boolean isStrongNumber(int num) {
+        int temp = num;
+        int sum = 0;
+
+        while (temp != 0) {
+            int digit = temp % 10;
+            sum += factorial(digit);
+            temp /= 10;
+        }
+        return sum == num;
+    }
+
+    // ---------- FACTORS ----------
+    public static int[] getFactors(int num) {
+        int count = 0;
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) count++;
+        }
+
+        int[] factors = new int[count];
+        int index = 0;
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                factors[index++] = i;
+            }
+        }
+        return factors;
+    }
+
+    public static int greatestFactor(int[] factors) {
+        int max = Integer.MIN_VALUE;
+        for (int f : factors) {
+            if (f > max) max = f;
+        }
+        return max;
+    }
+
+    public static int sumOfFactors(int[] factors) {
+        int sum = 0;
+        for (int f : factors) sum += f;
+        return sum;
+    }
+
+    public static int productOfFactors(int[] factors) {
+        int product = 1;
+        for (int f : factors) product *= f;
+        return product;
+    }
+
+    public static double productOfCubeOfFactors(int[] factors) {
+        double product = 1;
+        for (int f : factors) {
+            product *= Math.pow(f, 3);
+        }
+        return product;
+    }
+
+    // ---------- OTP ----------
+    public static int generateOTP() {
+        return (int)(Math.random() * 900000) + 100000;
+    }
+
+    public static boolean areOTPsUnique(int[] otps) {
+        for (int i = 0; i < otps.length; i++) {
+            for (int j = i + 1; j < otps.length; j++) {
+                if (otps[i] == otps[j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
