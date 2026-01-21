522172d02033a80e576919ffa39decd986ab9123
Shivani Sharma <shivanisharma3081@gmail.com>
2026-01-21 16:49:41 +0530

    [Shivani]Add.Collections

---
+++ collections-practice/gcr-codebase/Collections/SetInterface/UnionAndIntersaction.java
@@ -0,0 +1,24 @@
+package SetInterface;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class UnionAndIntersaction {
+
+	public static void main(String[] args) {
+		Set<Integer> set1 = new HashSet<>(Arrays.asList(1,2,3));
+		Set<Integer> set2 = new HashSet<>(Arrays.asList(3,4,5));
+		// UNION
+        Set<Integer> union = new HashSet<>(set1);
+        union.addAll(set2);
+
+        // INTERSECTION
+        Set<Integer> intersection = new HashSet<>(set1);
+        intersection.retainAll(set2);
+
+        System.out.println("Union: " + union);
+        System.out.println("Intersection: " + intersection);
+		
+	}
+}
